ca3f43be233799faec9d166cb67f659f184ecd81
unknown <arulselviusp@gmail.com>
2022-01-05 11:46:28 +0530

    new commit

---
+++ spotify/src/removesong.js
@@ -0,0 +1,27 @@
+let getdata = async (url, uri) => {
+  let data = await fetch(url, {
+    method: "DELETE",
+    body: JSON.stringify({
+      tracks: [
+        {
+          uri: `${uri}`,
+        },
+      ],
+    }),
+  });
+  data = await data.json();
+  console.log(data);
+  return data;
+};
+let removesong = (user_id, p_id, access_token, uri) => {
+  let url = `https://api.spotify.com/v1/playlists/${p_id}/tracks?access_token=${access_token}`;
+  getdata(url, uri)
+    .then((data) => {
+      console.log(data);
+      document.getElementById(`${p_id}`).click();
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+export default removesong;
